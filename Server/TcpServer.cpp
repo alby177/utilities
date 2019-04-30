4259229de618fb092ae2449aa2747f0d8b51f9d9
Alberto Gotta <gotta.alberto@gmail.com>
2019-05-01 00:43:20 +0200

    Developed TcpServer class constructor
    TcpServer: Added socket creation and binding from object constructor
    TcpServer: Added error structure for error string and code handling
    Signed-off-by: Alberto Gotta <gotta.alberto@gmail.com>

---
+++ Server/TcpServer.cpp
@@ -0,0 +1,51 @@
+#include "TcpServer.h"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+TcpServer::TcpServer(int port, ErrorStruct *err, int maxClients)
+{
+	struct sockaddr_in servaddr;
+  bool yes = true;
+
+  // Save port number
+  mPort = port;
+
+  // Create socket
+  if ((mServerSock = socket( AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    *err << "Error creating socket";
+    *err << errCodCreate;
+  }
+
+  // Set socket for binding already binded socket
+  if(setsockopt(SOL_SOCKET, SO_REUSEADDR, SO_KEEPALIVE, &yes, sizeof(int)) < 0)
+  {
+    // "Error setting socket settings" >> *err;
+    // *err << errCodSettings;
+  }
+
+  // Set memory structure for TCP/IP connection
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(mPort);
+
+	// Bind created socket
+	if (bind(mServerSock, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
+  {
+    *err << "Error binding socket" ;
+    *err << errCodBind;
+	}
+
+	// Set socket to listen
+	if (listen(mServerSock, maxClients) < 0)
+  {
+    *err << "Error listening from socket";
+    *err << errCodListen;
+  }
+}
--- Server/TcpServer.h
+++ Server/TcpServer.h
@@ -1,20 +1,45 @@
 #ifndef TCPSERVER_H
 #define TCPSERVER_H
+#include <string>
+
+enum ErrorCodes
+{
+  errCodCreate,               // Error creating socket
+  errCodSettings,             // Error setting socket options
+  errCodBind,                 // Error binding socket
+  errCodListen,               // Error putting socket on listening
+};
+
+struct ErrorStruct
+{
+private:
+  ErrorCodes errCode;
+  std::string errString;
+
+public:
+  //std::ostream &operator << (std::ostream &out) {out << errString << ". With error code: " << errCode; return out;}
+  void operator << (const std::string in) {errString = in;}
+  void operator << (const char *in) {errString = in;}
+  void operator << (const ErrorCodes &in) {errCode = in;}
+  void operator << (const int &in) {errCode = static_cast<ErrorCodes>(in);}
+};
 
 class TcpServer
 {
 public:
-  TcpServer();
-  ~TcpServer();
-  int Connect(int port = 0);
-  int AddClient();
+  TcpServer(int port, ErrorStruct *err = nullptr, int maxClients = 5);
+  ~TcpServer(){}
+  int AddClient(void *clientFunction, void *clientData = nullptr);  // Run connected client code
+  const inline int GetPortNumber() {return mPort;}  // Get port number
 
 private:
   // Private methods
-  
+
 
   // private members
-  int mPort = 0;
-}
+  int mPort = 0;            // Port number
+  int mServerSock = 0;      // Server socket descriptor
+  int mClientConnected = 0; // Number of conected clients
+};
 
 #endif
